540c4cc376ee45e26ae90c720d89e34e3605f6b8
marileejean <marilee02@gmail.com>
2022-12-05 20:31:20 -0600

    fixes to img slider

--- imggallery.js
+++ imggallery.js
@@ -4,14 +4,12 @@ showGallery(currentImg);
 function showGallery(num){
     var x;
     var images = document.getElementsByClassName("galleryImage");
-    if (num > images.length)
-        {currentImg = 1}
-    if (num < 1)
-        {currentImg = slides.length}
+    if (num > images.length){currentImg = 1}
+    if (num < 1){currentImg = slides.length}
     for (x = 0; x < images.length; x++){
         images[x].style.display = "none";
     }
-    images[currentIndex - 1].style.display = "block";
+    images[currentImg - 1].style.display = "block";
 }
 
 function setImage(num){
